885fb42a4aa0e346927f1b9138bd4a77a4c10aa2
QobilovOzodbek <ozodbekqobilovinfo@gmail.com>
2026-01-17 21:03:42 +0500

    Restore contact section inclusion in index.php

--- index.php
+++ index.php
@@ -4,6 +4,6 @@
 <?php include 'sections/about.php'; ?>
 <?php include 'sections/skills.php'; ?>
 <?php include 'sections/projects.php'; ?>
-<?php include 'sections/contact.php'; ?> //form bor teg
+<?php include 'sections/contact.php'; ?>
 
 <?php include 'footer.php'; ?>
