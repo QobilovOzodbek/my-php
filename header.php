da7d847b9a7d28ebfdbc0bc2cd2b2796b270baf5
QobilovOzodbek <ozodbekqobilovinfo@gmail.com>
2026-01-17 21:01:44 +0500

    Add missing stylesheet and script links in header.php

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap" rel="stylesheet">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css?v=<?= time() ?>">
 </head>
 
 <body>
@@ -23,3 +24,4 @@
             <a href="#contact">Contact</a>
         </nav>
     </header>
+    <script src="assets/js/main.js?v=<?= time() ?>"></script>
